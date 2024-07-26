2690cce7892b950df99b98c0d9a3f65a10643bd7
gitfanlab <mathis.togni@sfr.fr>
2024-07-26 09:25:19 +0200

    maj

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -1,16 +1,21 @@
 <?php
 namespace App\Http\Controllers;
+
 use App\Models\Image;
 use Illuminate\Http\Request;
+
 class ImageController extends Controller
 {
     public function store(Request $request)
     {
         $request->validate(['image' => 'required|image']);
         $path = $request->file('image')->store('images', 'public');
+    
         $image = new Image;
         $image->path = $path;
         $image->save();
+    
+        // Stocker le chemin de l'image dans la session
         return back()->with('imagePath', $path);
     }
   
